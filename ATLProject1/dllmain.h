0c9f95ce5e56a5dd37d0fa15aaa1c84cd753c6b3
Bear <1070458158@qq.com>
2025-06-20 18:06:08 +0800

    1

---
+++ ATLProject1/dllmain.h
@@ -0,0 +1,10 @@
+﻿// dllmain.h: 模块类的声明。
+
+class CATLProject1Module : public ATL::CAtlDllModuleT< CATLProject1Module >
+{
+public :
+	DECLARE_LIBID(LIBID_ATLProject1Lib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_ATLPROJECT1, "{9bb09634-6743-4d98-a512-d8b66764f71c}")
+};
+
+extern class CATLProject1Module _AtlModule;
